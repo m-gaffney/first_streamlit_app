39402792956eb053cd024e9d38ce0918704fefed
Maria Gaffney <116592223+m-gaffney@users.noreply.github.com>
2022-10-25 10:26:27 -0400

    Update streamlit_app.py
    Import pandas
    Import file
    Display dataframe from file

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,10 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+
+# Import file
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Display dataframe
+streamlit.dataframe(my_fruit_list)
